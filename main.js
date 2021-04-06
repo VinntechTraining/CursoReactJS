2662b9f95373423c958ffef76ff5b20b1ed77491
GabRoca <ing.gabriel.romero@gmail.com>
2021-04-05 21:03:58 -0400

    Preparacion previa a ReactJS 1

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-'use strict';
+/*'use strict';*/
+
+const servivios = {
+    api: {},
+    mailer: 'soy mailer',
+    apollo: {},
+
+}
+
+const enviarCorreo = () => {
+
+    console.log(mailer);
+    //redactar texto de correo
+}
