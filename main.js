02cff8aaed2060ea6f4b149e3a220c190352ff1c
GabRoca <ing.gabriel.romero@gmail.com>
2021-04-07 20:52:22 -0400

    ReactJS 1

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 /*'use strict';*/
 
-const servivios = {
+const servicios = {
     api: {},
     mailer: 'soy mailer',
     apollo: {},
 
 }
 
-const enviarCorreo = () => {
+const enviarCorreo = (srs) => {
 
+    const { mailer } = srs
     console.log(mailer);
     //redactar texto de correo
 
 
 }
+
+enviarCorreo(servicios)
